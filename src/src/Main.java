d71abc35a6e7f2949de2c54275da75e57b288759
Vladimir Kuzmin <vovkunch@gmail.com>
2025-10-08 12:50:15 +0500

    Изменил проверку в main

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,13 +3,43 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         MyHashMap<Integer, String> map = new MyHashMap<>();
-        map.put(0, "zero");
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        System.out.println(map.get(0));
-        System.out.println(map.remove(1));
+
+        System.out.println("Проверка put");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(map.put(i, String.valueOf(i)) + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(map.table));
+        System.out.println();
+
+        System.out.println("Проверка перезаписи значения");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(map.put(i, String.valueOf(i+10)) + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(map.table));
+        System.out.println();
+
+        System.out.println("Проверка get");
+        for (int i = 0; i < map.table.length; i++) {
+            System.out.print(map.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(map.table));
+        System.out.println();
+
+        System.out.println("Проверка remove");
+        for (int i = 0; i < map.table.length; i++) {
+            System.out.print(map.remove(i) + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(map.table));
+        System.out.println();
+
+        System.out.println("Проверка resize");
+        for (int i = 0; i < 20; i++) {
+            map.put(i, String.valueOf(i));
+        }
         System.out.println(Arrays.toString(map.table));
     }
 }
